0b097c9c337aaf78246b603bfbcfeb943d3d15e5
Lakshay Newatia <141578754+lakshayNewatia@users.noreply.github.com>
2026-03-12 01:35:25 +0530

    Update auth.api.js

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
-
+const baseURL = import.meta.env.VITE_API_URL || "http://localhost:3000";
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: baseURL,
     withCredentials: true
 })
 
