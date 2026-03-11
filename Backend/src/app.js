0b3da551f24b2a830b91683b21462fa87c57cb46
Lakshay Newatia <141578754+lakshayNewatia@users.noreply.github.com>
2026-03-12 01:15:30 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://interview-hxuv4txw4-lakshaynewatias-projects.vercel.app/",
     credentials: true
 }))
 
