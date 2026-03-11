f37e16890a4fa8cbc5156ea0c5da2471210199b8
Lakshay Newatia <141578754+lakshayNewatia@users.noreply.github.com>
2026-03-12 01:13:39 +0530

    Update interview.api.js

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://interview-ai-jzo6.onrender.com",
     withCredentials: true,
 })
 
